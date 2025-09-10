e93203b78a30365708a1215ee73f8a079c403584
dantad04 <86401447+dantad04@users.noreply.github.com>
2025-09-09 20:26:44 -0600

    Add files via upload
    added coins as healthpacks (they restore health) and tested out lighting on the gun

--- player.gd
+++ player.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 signal health_depleted
 
+@onready var coin_label = %Label
 var health = 100.0
+
 
 var coin_counter = 0 
 func _physics_process(delta):
@@ -29,6 +31,8 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Coins"):
 		set_coin(coin_counter + 1)
 		print(coin_counter)
+		health += 1
 
 func set_coin(new_coin_count: int) -> void:
 	coin_counter = new_coin_count
+	coin_label.text = "Coin Count : " + str(coin_counter)
